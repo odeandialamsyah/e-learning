e904462998f58e36a29c718bfec83963d9f72748
odeandialamsyah <odeandialamsyah@gmail.com>
2024-06-16 03:07:58 +0700

    route regis

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+
+
 
 Route::get('/', function () {
     return view('index');
 });
 
 
+//register
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/registerAdd', [RegisterController::class, 'register']);
+
+
 //login
-Route::get('login', [LoginController::class, 'index'])->name('login');
-Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/loginApp', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
