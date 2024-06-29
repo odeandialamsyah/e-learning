eca0f3f6f0f8e182acd6c825a9464c52137e6ab1
odeandialamsyah <odeandialamsyah@gmail.com>
2024-06-29 21:21:30 +0700

    sid

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -5,7 +5,7 @@
     </a>
     <ul class="mt-4">
         <li class="mb-1 group active">
-            <a href="{{ route('dashboard') }}"
+            <a href="{{ route('admin.dashboard') }}"
                 class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
                 <i class="ri-home-2-line mr-3 text-lg"></i>
                 <span class="text-sm">Dashboard</span>
@@ -15,7 +15,7 @@
         <li class="mb-1 group">
             <a href="{{route('users.index')}}"
                 class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class="fa-regular fa-newspaper mr-3 text-lg"></i>
+                <i class="bi bi-person-lines-fill mr-3 text-lg"></i>
                 <span class="text-sm">Users</span>
             </a>
         </li>
@@ -50,10 +50,24 @@
         </li>
         <li class="mb-1 group">
             <a href="#"
-                class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
+                class="flex items-center py-2 px-4 text-gray-300 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
                 <i class="ri-settings-2-line mr-3 text-lg"></i>
-                <span class="text-sm">Settings</span>
+                <span class="text-sm">Setting</span>
+                <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
             </a>
+            <ul class="pl-7 mt-2 hidden group-[.selected]:block">
+                <li class="mb-4">
+                    <a href="{{ route('dashboard') }}"
+                        class="text-gray-300 text-sm flex items-center hover:text-gray-100 before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">
+                        Dashboard Users</a>
+                </li>
+            </ul>
+            <ul class="pl-7 mt-2 hidden group-[.selected]:block">
+                <li class="mb-4"><a href="{{ route('logout') }}" class="text-white" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+            </ul>
         </li>
     </ul>
 </div>
